882a82fee1c9dd4caa43600d8bfbcb2caa0e75a8
Ahmed Bourhazi <medbourhazi1234@gmail.com>
2025-01-09 20:46:31 +0100

    feat: add upload service utility for handling API calls

--- lib/utils/upload_service.dart
+++ lib/utils/upload_service.dart
@@ -17,11 +17,10 @@ class ApiService {
     try {
       var uri = Uri.parse("$baseUrl/upload_cin");
       var request = http.MultipartRequest('POST', uri);
-      request.headers['Authorization'] = 'Bearer $token';  // Ajouter JWT pour sécurité
+      request.headers['Authorization'] = 'Bearer $token';
 
       http.MultipartFile multipartFile;
 
-      // Upload pour Web
       if (kIsWeb && webImage != null) {
         var mimeType = lookupMimeType('', headerBytes: webImage) ?? 'application/octet-stream';
         multipartFile = http.MultipartFile.fromBytes(
@@ -30,9 +29,7 @@ class ApiService {
           contentType: MediaType.parse(mimeType),
           filename: 'upload.png',
         );
-      } 
-      // Upload pour Mobile / Desktop
-      else if (image != null) {
+      } else if (image != null) {
         var mimeType = lookupMimeType(image.path) ?? 'application/octet-stream';
         multipartFile = await http.MultipartFile.fromPath(
           'image',
@@ -43,35 +40,23 @@ class ApiService {
         return 'No image selected!';
       }
 
-      // Ajouter le fichier à la requête
       request.files.add(multipartFile);
 
-      // Envoi de la requête
       var response = await request.send();
       var responseBody = await response.stream.bytesToString();
 
-      if (response.statusCode == 200 || response.statusCode == 201) {
-        // Extraction de la partie JSON brute
-        final dataString = responseBody;
-        final startIndex = dataString.indexOf('{');
-        final endIndex = dataString.lastIndexOf('}');
+      // Ajouter ce print pour voir la réponse brute
+      print("Réponse brute du serveur : $responseBody");
 
-        if (startIndex != -1 && endIndex != -1) {
-          try {
-            final jsonString = dataString.substring(startIndex, endIndex + 1);
-            final data = jsonDecode(jsonString);
-            return " ${data}";
-          } catch (e) {
-            return "Error decoding data: $e";
-          }
-        } else {
-          return "Invalid response format!";
-        }
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        return responseBody;
       } else {
         return "Upload failed! Status: ${response.statusCode}";
       }
     } catch (e) {
+      print("Erreur lors de l'upload : $e");
       return "Error during upload: $e";
     }
   }
+
 }
